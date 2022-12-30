047a0681ca074fdf853badc886713e7bdaa219d3
blipntime <expert__@verne.us>
2022-12-30 15:37:56 -0800

    trigger on pull request agains main (#1)
    * trigger on pull request agains main
    
    * other commit
    
    * always failing action
    
    * always failing action with process.exit(1)
    
    * 2 workflow test
    
    Co-authored-by: János Mucsi <janos.mucsi@pragmaticruby.net>

--- bin/processfile.js
+++ bin/processfile.js
@@ -1,5 +1,14 @@
 const fs = require('fs')
+const core = require('@actions/core')
 
-const contents = fs.readFileSync('somefile.output', 'utf8')
+let contents
+try {
+  contents = fs.readFileSync('somefile.output', 'utf8')
+} catch (err) {
+  core.setFailed(`Action failed with error ${err}`)
+}
 
-console.log(contents, "\nDone")
+console.log(contents, "\nProcess file Done")
+
+//
+// process.exit(1);
