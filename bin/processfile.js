b348013bc46969888f2d6910565439449ec718f8
János Mucsi <janos.mucsi@pragmaticruby.net>
2022-12-30 15:17:12 -0800

    always failing action

--- bin/processfile.js
+++ bin/processfile.js
@@ -1,5 +1,8 @@
 const fs = require('fs')
+const core = require('@actions/core')
 
 const contents = fs.readFileSync('somefile.output', 'utf8')
 
 console.log(contents, "\nDone")
+
+core.setFailed(`Action failed with error`)
